40e0cfd2944b541f6b8480a643f1781ad04aa0f3
Даниил Зайцев <107057557+tyansao@users.noreply.github.com>
2024-02-29 19:06:55 +0400

    Add files via upload

---
+++ sauce.cy.js
@@ -0,0 +1,23 @@
+describe('GlobalsQA Test', () => {
+
+    beforeEach(() => {
+        cy.visit('https://www.globalsqa.com/')
+    })
+
+    it('should add a product to the cart', () => {
+        // Write your test steps here
+    })
+
+    it('should verify the items in the cart', () => {
+        // Write your test steps here
+    })
+
+    it('should verify the user authentication process', () => {
+        // Write your test steps here
+    })
+
+    it('should create a new account', () => {
+        // Write your test steps here
+    })
+
+})
